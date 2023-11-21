4ca9f015ecaad450f6319acf133992793203df05
soyyyyy <da5868@naver.com>
2023-11-22 05:07:32 +0900

    ft.상세페이지 작업1

---
+++ src/components/Detail/Reviewdata.js
@@ -0,0 +1,18 @@
+export const ReviewData = [
+    {
+        id: 1,
+        username: 'User1',
+        rating: 2,
+        date: '2023-01-01',
+        content: '오늘은 오늘의 메뉴 A에 닭다리가 나왔는데 너무 짜요 최악이네요 다신 먹지 않겠어요.',
+        image: '/bamsik-image.jpg',
+    },
+    {
+        id: 2,
+        username: 'User2',
+        rating: 5,
+        date: '2023-01-02',
+        content: '굳. 든든해요',
+        image: '/bamsik-image.jpg',
+    },
+];
