7b434c23b2f11c225b1630e4aa9a83aca79901fd
soyyyyy <da5868@naver.com>
2023-11-29 15:23:26 +0900

    새로고침해도 로그아웃안되게 수정

--- src/components/Login/AuthContext.jsx
+++ src/components/Login/AuthContext.jsx
@@ -1,111 +1,62 @@
-import * as L from './LoginStyle';
-import LogoImage from './logo.png';
-import * as C from './../Main/ContainerStyle';
-import { Link, useNavigate } from 'react-router-dom'; // useNavigate instead of useHistory
-import { useState } from 'react';
-import { useAuth } from './AuthContext';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { createContext, useContext, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-export default function Login() {
-  const { login } = useAuth();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // useNavigate instead of useHistory
-  const handleLogin = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/app/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-      });
-  
-      const data = await response.json();
-    
-      if (response.ok && data.code === 200) {
-        // 토큰 위치와 구조에 따라 수정
-        const authToken = data.result.jwt;
-  
-        if (authToken) {
-          localStorage.setItem('authToken', authToken);
-          login();
-          toast.success('로그인 성공!', {
-            autoClose: 3000,
-            position: toast.POSITION.TOP_CENTER,
-          });
-          navigate('/');
-          console.log('Your Auth Token:', authToken);
+const AuthContext = createContext();
 
-        } else {
-          console.error('로그인 실패: 토큰이 정상적으로 반환되지 않았습니다.');
-          toast.error('로그인 실패. 이메일과 비밀번호를 확인해 주세요', {
-            autoClose: 3000,
-            position: toast.POSITION.TOP_CENTER,
-          });
-        }
-      } else {
-        console.error('로그인 실패:', data);
-        toast.error('로그인 실패. 이메일과 비밀번호를 확인해 주세요', {
-          autoClose: 3000,
-          position: toast.POSITION.TOP_CENTER,
-        });
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      toast.error('로그인 중 에러가 발생했습니다. 다시 시도해 주세요', {
-        autoClose: 3000,
-        position: toast.POSITION.TOP_CENTER,
-      });
+export const AuthProvider = (props) => {
+  const { children } = props;
+
+  // 초기 상태는 로컬 스토리지에서 값을 가져오거나 기본값으로 설정
+  const initialLoggedInState = localStorage.getItem('isLoggedIn') === 'true';
+  const [isLoggedIn, setLoggedIn] = useState(initialLoggedInState);
+
+  const initialUserProfileState = JSON.parse(localStorage.getItem('userProfile')) || {};
+  const [userProfile, setUserProfile] = useState(initialUserProfileState);
+
+  const initialUserId = localStorage.getItem('userId') || null;  // 새로고침 이전 로그인 되었는지 확인
+  const [userId, setUserId] = useState(initialUserId);
+
+  const login = () => {
+    // 로그인 로직 수행
+    setLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const logout = () => {
+    // 로그아웃 로직 수행
+    setLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+    // 추가: 로그아웃 시 사용자 정보 초기화
+    setUserProfile({});
+    localStorage.removeItem('userProfile');
+    setUserId(null);
+    localStorage.removeItem('userId');
+  };
+
+  const updateUser = (userInfo) => {
+    setUserProfile(userInfo);
+    if (userInfo && userInfo.id) {
+      setUserId(userInfo.id);
+      localStorage.setItem('userId', userInfo.id);
     }
   };
-  
+
+  useEffect(() => {
+    // Save changes to localStorage whenever the state changes
+    localStorage.setItem('userProfile', JSON.stringify(userProfile));
+  }, [userProfile]);
 
   return (
-    <C.Container>
-      <C.WhiteBox>
-        <L.ContentContainer>
-          <L.BackButton as={Link} to="/">⬅ BACK TO MENU</L.BackButton>
-          <L.LogoContainer>
-            <L.LogoImage src={LogoImage} alt="로고" />
-          </L.LogoContainer>
-
-          <L.Title>LogIn</L.Title>
-          <L.IvoryBox>
-            <L.LoginRow>
-              <L.LoginContent>EMAIL</L.LoginContent>
-              <L.InputBox
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </L.LoginRow>
-            <L.LoginRow>
-              <L.LoginContent>PASSWORD</L.LoginContent>
-              <L.InputBox
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </L.LoginRow>
-
-            <L.LoginButton
-              onClick={handleLogin}
-              disabled={!email || !password}
-            >
-              LogIn
-            </L.LoginButton>
-
-          </L.IvoryBox>
-          <L.SigninButton as={Link} to="/signin">
-            회원가입
-          </L.SigninButton>
-        </L.ContentContainer>
-      </C.WhiteBox>
-    </C.Container>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, userProfile, updateUser, userId }}>
+      {children}
+    </AuthContext.Provider>
   );
-}
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
