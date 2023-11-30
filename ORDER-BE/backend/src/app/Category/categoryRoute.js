89afd933d916395178ccaaff8c8262f4ed91b1ef
soyyyyy <da5868@naver.com>
2023-11-30 22:13:31 +0900

    백엔드 다시 연결하려고

--- ORDER-BE/backend/src/app/Category/categoryRoute.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(app){
-    const category = require('./categoryController');
-    //category 전체 조회
-    app.get('/app/getcategoryList',category.getCategoryList);
-    //특정 category 조회
-    app.get('/app/getcategory/:id', category.getCategory);
-    
-}
